43c29d4639df38cf9dcefafe0b714fdc818f5715
Reinder <reinder.devries@hu.nl>
2023-12-18 10:11:50 +0100

    Added deployment bash script

---
+++ deploy.sh
@@ -0,0 +1,31 @@
+# init local variables
+DEPLOY_FOLDER="dist"
+GIT_COMMIT_MESSAGE="New build for deploy: "$(date)
+GIT_BRANCH_LOCAL="deploy"
+GIT_BRANCH_REMOTE="gh-pages"
+GIT_REPO_REMOTE="https://github.com/reinder42/SV2FE-todo-app"
+
+# abort on errors
+set -e
+# First run the build command as usual
+npm run build
+# navigate into the build output folder (default this is dist/)
+cd $DEPLOY_FOLDER
+# copy index.html to 404.html
+# to fix 404 errors on navigating to a route directly
+cp index.html 404.html
+# create empty .nojekyll file to bypass Jekyll processing (a gitHub thing)
+echo > .nojekyll
+
+# initialize git IN THE DIST FOLDER (we navigated here above)
+git init
+# if not exists create a new local branche called 'deploy'
+# otherwise reset the branch
+git checkout -B $GIT_BRANCH_LOCAL
+# add all files to the git index
+git add -A
+# commit all changes with the message 'deploy'
+git commit -m "$GIT_COMMIT_MESSAGE"
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f $GIT_REPO_REMOTE $GIT_BRANCH_LOCAL:$GIT_BRANCH_REMOTE
+
